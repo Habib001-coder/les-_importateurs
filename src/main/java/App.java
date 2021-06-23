fe2b991fc89b3ff3541f580be17c787ad91ee5df
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-24 02:51:20 +0300

    Add the ShippingDetails logic and functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,44 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        // Map<String,Object> model = new HashMap<String,Object>();
+        String layout = "templates/layout.hbs";
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("templates", "template/homepage.hbs");
+            return new ModelAndView(model, "homepage.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/details",(request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            String flightNumber=request.queryParams("flightNumber");
+            Integer arrivalTime=Integer.parseInt(request.queryParams("arrivalTime"));
+            Integer customDuty=Integer.parseInt(request.queryParams("customDuty"));
+            Integer containerNumber=Integer.parseInt(request.queryParams("containerNumber"));
+            ShippingDetails shipDetails = new ShippingDetails(flightNumber,arrivalTime,customDuty,containerNumber);
+            model.put("shipDetails",shipDetails);
+            return new ModelAndView(model, "details-view.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        post("/details/new",(request, response) -> {
+            Map<String,Object> model = new HashMap<>();
+            String flightNumber=request.queryParams("flightNumber");
+            Integer arrivalTime=Integer.parseInt(request.queryParams("arrivalTime"));
+            Integer customDuty=Integer.parseInt(request.queryParams("customDuty"));
+            Integer containerNumber=Integer.parseInt(request.queryParams("containerNumber"));
+            ShippingDetails shipDetails = new ShippingDetails(flightNumber,arrivalTime,customDuty,containerNumber);
+            model.put("shipDetails",shipDetails);
+            return new ModelAndView(model, "details-view.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
--- src/main/java/ShippingDetails.java
+++ src/main/java/ShippingDetails.java
@@ -1,2 +1,48 @@
 public class ShippingDetails {
+    private String flightNumber;
+    private int arrivalTime;
+    private int customDuty;
+    private int containerNumber;
+
+    public ShippingDetails(String flightNumber,int arrivalTime,int customDuty,int containerNumber){
+        this.flightNumber=flightNumber;
+        this.arrivalTime=arrivalTime;
+        this.customDuty=customDuty;
+        this.containerNumber=containerNumber;
+
+
+
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getCustomDuty() {
+        return customDuty;
+    }
+
+    public void setCustomDuty(int customDuty) {
+        this.customDuty = customDuty;
+    }
+
+    public int getContainerNumber() {
+        return containerNumber;
+    }
+
+    public void setContainerNumber(int containerNumber) {
+        this.containerNumber = containerNumber;
+    }
 }
