86d5c5a3b6d2e11bfe65acd613965b5acef68287
Abdallah@moringa <abdallah.abdul@student.moringaschool.com>
2021-06-24 08:43:52 +0300

    make soome changes

--- src/main/java/GoodsDetails.java
+++ src/main/java/GoodsDetails.java
@@ -1,6 +1,49 @@
 public class GoodsDetails {
-    @Override
-    public String toString() {
-        return super.toString( );
+    private  String name;
+    private  String serial;
+    private  String quantity;
+    private  String origin;
+
+    public  GoodsDetails(String name, String serial, String quantity, String origin){
+        this.name= name;
+        this.serial= serial;
+        this.quantity=quantity;
+        this.origin=origin;
+
+
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public String getSerial() {
+        return serial;
+    }
+
+    public void setSerial( String serial ) {
+        this.serial = serial;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity( String quantity ) {
+        this.quantity = quantity;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin( String origin ) {
+        this.origin = origin;
     }
 }
