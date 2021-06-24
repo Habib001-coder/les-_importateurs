26f2fcd7f85178ea44c05076a1f3680907daccb5
Allantoizmughus <allan.mwangi@student.moringaschool.com>
2021-06-25 02:23:10 +0300

    Add some styling to the homepage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,7 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,19 +26,13 @@ public class App {
         //display details form
         get("/details-form", (request, response)->{
             Map<String, Object> model=new HashMap<>();
-            return new ModelAndView(model, "details-form.hbs");
+            return new ModelAndView(model, "details-forms.hbs");
         }, new HandlebarsTemplateEngine());
 
 
         //process details-form
         get("/details",(request, response) -> {
             Map<String,Object> model = new HashMap<>();
-            String flightNumber=request.queryParams("flightNumber");
-            Integer arrivalTime=Integer.parseInt(request.queryParams("arrivalTime"));
-            Integer customDuty=Integer.parseInt(request.queryParams("customDuty"));
-            Integer containerNumber=Integer.parseInt(request.queryParams("containerNumber"));
-            ShippingDetails shipDetails = new ShippingDetails(flightNumber,arrivalTime,customDuty,containerNumber);
-            model.put("shipDetails",shipDetails);
             return new ModelAndView(model, "details-forms.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -45,12 +40,6 @@ public class App {
         //display details
         post("/details/new",(request, response) -> {
             Map<String,Object> model = new HashMap<>();
-            String flightNumber=request.queryParams("flightNumber");
-            Integer arrivalTime=Integer.parseInt(request.queryParams("arrivalTime"));
-            Integer customDuty=Integer.parseInt(request.queryParams("customDuty"));
-            Integer containerNumber=Integer.parseInt(request.queryParams("containerNumber"));
-            ShippingDetails shipDetails = new ShippingDetails(flightNumber,arrivalTime,customDuty,containerNumber);
-            model.put("shipDetails",shipDetails);
             return new ModelAndView(model, "details-views.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -71,12 +60,10 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
         //display reviews
-        get("/reviews/new",(request, response) -> {
+        get("/reviews-forms",(request, response) -> {
             Map<String,Object> model = new HashMap<>();
-            String name=request.queryParams("name");
-            String comment=request.queryParams("comment");
-            Reviews newReviews = new Reviews(name,comment);
-            model.put("newReviews",newReviews);
+            ArrayList<Reviews> reviews= Reviews.getAllInstances();
+            model.put("reviews",reviews);
             return new ModelAndView(model, "reviews-forms.hbs");
         }, new HandlebarsTemplateEngine());
 
--- src/main/java/Reviews.java
+++ src/main/java/Reviews.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+
 public class Reviews {
     private String name;
     private String comment;
+    private static ArrayList<Reviews> instances=new ArrayList<>();
+    private int id;
 
     public Reviews(String name,String comment){
         this.name=name;
         this.comment=comment;
+        instances.add(this);
+        this.id=instances.size();
+
     }
 
 
@@ -24,7 +31,12 @@ public class Reviews {
         this.comment = comment;
     }
 
-    public static Reviews setUpReview1(){
+    public static ArrayList<Reviews> getAllInstances() {
+        return instances;
+    }
+
+
+        public static Reviews setUpReview1(){
         return new Reviews("Sesan","Good services");
     }
     public static Reviews setUpReview2(){
